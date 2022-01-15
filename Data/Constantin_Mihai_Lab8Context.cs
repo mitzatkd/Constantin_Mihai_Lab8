45461c18b3fd119737bd0074301aed0abe53e711
Constantin Mihai <mitza_tkd@yahoo.com>
2022-01-15 17:32:35 +0200

    Add project files.

---
+++ Data/Constantin_Mihai_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Constantin_Mihai_Lab8.Models;
+
+namespace Constantin_Mihai_Lab8.Data
+{
+    public class Constantin_Mihai_Lab8Context : DbContext
+    {
+        public Constantin_Mihai_Lab8Context (DbContextOptions<Constantin_Mihai_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Constantin_Mihai_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Constantin_Mihai_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Constantin_Mihai_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Details.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Constantin_Mihai_Lab8.Data;
+using Constantin_Mihai_Lab8.Models;
+
+namespace Constantin_Mihai_Lab8.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Constantin_Mihai_Lab8.Data.Constantin_Mihai_Lab8Context _context;
+
+        public DetailsModel(Constantin_Mihai_Lab8.Data.Constantin_Mihai_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Book = await _context.Book
+                .Include(b=>b.Publisher)
+                .Include(b=>b.BookCategories)
+                .ThenInclude(b=>b.Category)
+                .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
